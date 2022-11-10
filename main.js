7fffdaa945efe064fd751d8163c772d03c7f2b83
Svante Pålsson <svantepalsson1987@gmail.com>
2022-11-10 16:49:30 +0100

    zooom

--- main.js
+++ main.js
@@ -7,23 +7,6 @@ window.onload = () => {
         `latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude};`
       );
     console.log("hej");
-    var userMediaConstraints = {
-      audio: false,
-      video: {
-        facingMode: "environment",
-        width: {
-          ideal: _this.parameters.sourceWidth,
-         // min: 1024,
-         // max: 1920,
-        },
-        height: {
-          ideal: _this.parameters.sourceHeight,
-          //min: 776,
-          //max: 1080,
-        },
-      },
-    };
-  
     // CUSTOM CODE START
     var backCam2 = devices.filter((d) => {
       console.log("kamera koll");
