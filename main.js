fedeb132775caa07f2e8037310711c7beb62ed99
Svante Pålsson <svantepalsson1987@gmail.com>
2022-11-10 15:40:10 +0100

    zoom work

--- main.js
+++ main.js
@@ -7,30 +7,29 @@ window.onload = () => {
         `latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude};`
       );
   });
+  var userMediaConstraints = {
+      audio: false,
+      video: {
+      facingMode: 'environment',
+      width: {
+      ideal: _this.parameters.sourceWidth,
+      min: 1024,
+      max: 1920
+      },
+      height: {
+      ideal: _this.parameters.sourceHeight,
+      min: 776,
+      max: 1080
+      }
+      }
+      }
+      
+      // CUSTOM CODE START
+      var backCam2 = devices.filter(d=>{
+      return d.label && d.label == "camera2 0, facing back";
+      })
+      if (backCam2.length) {
+      userMediaConstraints.video.deviceId = backCam2[0].deviceId
+      }
+      // CUSTOM CODE END
 };
-
-var userMediaConstraints = {
-    audio: false,
-    video: {
-    facingMode: 'environment',
-    width: {
-    ideal: _this.parameters.sourceWidth,
-    min: 1024,
-    max: 1920
-    },
-    height: {
-    ideal: _this.parameters.sourceHeight,
-    min: 776,
-    max: 1080
-    }
-    }
-    }
-    
-    // CUSTOM CODE START
-    var backCam2 = devices.filter(d=>{
-    return d.label && d.label == "camera2 0, facing back";
-    })
-    if (backCam2.length) {
-    userMediaConstraints.video.deviceId = backCam2[0].deviceId
-    }
-    // CUSTOM CODE END
