806c51511f57a593afd078488e051d934bd6abfc
Svante Pålsson <svantepalsson1987@gmail.com>
2022-11-10 17:41:24 +0100

    Nu ska man kunna byta kamera på sin androidtelefon

--- main.js
+++ main.js
@@ -8,32 +8,52 @@ window.onload = () => {
       );
   });
   // Välj kamera
-  const video = document.getElementById('video');
-  const button = document.getElementById('button');
-  const select = document.getElementById('select');
-  
-  button.addEventListener('click', event => {
+  const video = document.getElementById("video");
+  const button = document.getElementById("button");
+  const select = document.getElementById("select");
+  let currentStream;
+
+  function stopMediaTracks(stream) {
+    stream.getTracks().forEach((track) => {
+      track.stop();
+    });
+  }
+
+  button.addEventListener("click", (event) => {
+    if (typeof currentStream !== "undefined") {
+      stopMediaTracks(currentStream);
+    }
+    const videoConstraints = {};
+    if (select.value === "") {
+      videoConstraints.facingMode = "environment";
+    } else {
+      videoConstraints.deviceId = { exact: select.value };
+    }
     const constraints = {
       video: true,
-      audio: false
+      audio: false,
     };
+    
     navigator.mediaDevices
-      .getUserMedia(constraints)
-      .then(stream => {
-        video.srcObject = stream;
-      })
-      .catch(error => {
-        console.error(error);
-      });
+    .getUserMedia(constraints)
+    .then(stream => {
+      currentStream = stream;
+      video.srcObject = stream;
+      return navigator.mediaDevices.enumerateDevices();
+    })
+    .then(gotDevices)
+    .catch(error => {
+      console.error(error);
+    });
   });
 
   function gotDevices(mediaDevices) {
-    select.innerHTML = '';
-    select.appendChild(document.createElement('option'));
+    select.innerHTML = "";
+    select.appendChild(document.createElement("option"));
     let count = 1;
-    mediaDevices.forEach(mediaDevice => {
-      if (mediaDevice.kind === 'videoinput') {
-        const option = document.createElement('option');
+    mediaDevices.forEach((mediaDevice) => {
+      if (mediaDevice.kind === "videoinput") {
+        const option = document.createElement("option");
         option.value = mediaDevice.deviceId;
         const label = mediaDevice.label || `Camera ${count++}`;
         const textNode = document.createTextNode(label);
@@ -44,4 +64,3 @@ window.onload = () => {
   }
   navigator.mediaDevices.enumerateDevices().then(gotDevices);
 };
-
