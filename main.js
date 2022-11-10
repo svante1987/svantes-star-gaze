0eaa20a0e65e8da325b0d3f4395c053e6d603dd0
Svante Pålsson <svantepalsson1987@gmail.com>
2022-11-10 15:03:17 +0100

    mer krångel

--- main.js
+++ main.js
@@ -9,67 +9,28 @@ window.onload = () => {
   });
 };
 
-// get available devices
-navigator.mediaDevices.enumerateDevices().then(function(devices) {
-    var userMediaConstraints = {
-audio: false,
-video: {
+var userMediaConstraints = {
+    audio: false,
+    video: {
     facingMode: 'environment',
     width: {
-        ideal: _this.parameters.sourceWidth,
-        // min: 1024,
-        // max: 1920
+    ideal: _this.parameters.sourceWidth,
+    // min: 1024,
+    // max: 1920
     },
     height: {
-        ideal: _this.parameters.sourceHeight,
-        // min: 776,
-        // max: 1080
+    ideal: _this.parameters.sourceHeight,
+    // min: 776,
+    // max: 1080
     }
-  }
-}
-
-
-if (null !== _this.parameters.deviceId) {
-userMediaConstraints.video.deviceId = {
-    exact: _this.parameters.deviceId
-};
-}
-// get a device which satisfy the constraints
-navigator.mediaDevices.getUserMedia(userMediaConstraints).then(function success(stream) {
-// set the .src of the domElement
-domElement.srcObject = stream;
-var event = new CustomEvent('camera-init', {stream: stream});
-window.dispatchEvent(event);
-// to start the video, when it is possible to start it only on userevent. like in android
-document.body.addEventListener('click', function(){
-    domElement.play();
-});
-// domElement.play();
-             // TODO listen to loadedmetadata instead
-// wait until the video stream is ready
-var interval = setInterval(function() {
-    if (!domElement.videoWidth)	return;
-    onReady()
-    clearInterval(interval)
-}, 1000/50);
-}).catch(function(error) {
-onError({
-    name: error.name,
-    message: error.message
-});
-});
-}).catch(function(error) {
-onError({
-message: error.message
-});
-});
-
-// init controls for camera
-var markerControls = new THREEx.ArMarkerControls(arToolkitContext, camera, {
-    type: 'pattern',
-    patternUrl: THREEx.ArToolkitContext.baseURL + 'js/lib/data/patt.hiro',
-    // patternUrl : THREEx.ArToolkitContext.baseURL + '../data/data/patt.kanji',
-    // as we controls the camera, set changeMatrixMode: 'cameraTransformMatrix'
-    changeMatrixMode: 'cameraTransformMatrix',
-    deviceId: 2 // we need to let the user switch between available cameras here...
-})
+    }
+    }
+    
+    // CUSTOM CODE START
+    var backCam2 = devices.filter(d=>{
+    return d.label && d.label == "camera2 0, facing back";
+    })
+    if (backCam2.length) {
+    userMediaConstraints.video.deviceId = backCam2[0].deviceId
+    }
+    // CUSTOM CODE END
