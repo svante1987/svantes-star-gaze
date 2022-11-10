c519f61df6a7f13fc53e2b1718285d17ba691e23
Svante Pålsson <svantepalsson1987@gmail.com>
2022-11-10 17:31:32 +0100

    man kan starta med webcam och man kan se sina devices

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ window.onload = () => {
   // Välj kamera
   const video = document.getElementById('video');
   const button = document.getElementById('button');
+  const select = document.getElementById('select');
   
   button.addEventListener('click', event => {
     const constraints = {
@@ -25,5 +26,22 @@ window.onload = () => {
         console.error(error);
       });
   });
+
+  function gotDevices(mediaDevices) {
+    select.innerHTML = '';
+    select.appendChild(document.createElement('option'));
+    let count = 1;
+    mediaDevices.forEach(mediaDevice => {
+      if (mediaDevice.kind === 'videoinput') {
+        const option = document.createElement('option');
+        option.value = mediaDevice.deviceId;
+        const label = mediaDevice.label || `Camera ${count++}`;
+        const textNode = document.createTextNode(label);
+        option.appendChild(textNode);
+        select.appendChild(option);
+      }
+    });
+  }
+  navigator.mediaDevices.enumerateDevices().then(gotDevices);
 };
 
